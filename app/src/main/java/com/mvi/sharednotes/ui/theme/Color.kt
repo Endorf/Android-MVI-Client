fa2f0dc611f8e2188090c14dfe0a7758b0a14906
Endorf <dmytro.kosolapenko@gmail.com>
2023-03-07 13:20:16 +0200

    configure detekt

--- app/src/main/java/com/mvi/sharednotes/ui/theme/Color.kt
+++ app/src/main/java/com/mvi/sharednotes/ui/theme/Color.kt
@@ -2,7 +2,12 @@ package com.mvi.sharednotes.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// TODO: resolve magic numbers
+@Suppress("MagicNumber")
 val Purple200 = Color(0xFFBB86FC)
+@Suppress("MagicNumber")
 val Purple500 = Color(0xFF6200EE)
+@Suppress("MagicNumber")
 val Purple700 = Color(0xFF3700B3)
+@Suppress("MagicNumber")
 val Teal200 = Color(0xFF03DAC5)
