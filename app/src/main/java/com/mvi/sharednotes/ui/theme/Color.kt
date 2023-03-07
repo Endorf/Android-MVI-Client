ab0f98fd92be78fb87d80ee05c542ee9f189bad6
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-03-07 17:54:57 +0200

    configure Ktlint (#5)

--- app/src/main/java/com/mvi/sharednotes/ui/theme/Color.kt
+++ app/src/main/java/com/mvi/sharednotes/ui/theme/Color.kt
@@ -5,9 +5,12 @@ import androidx.compose.ui.graphics.Color
 // TODO: resolve magic numbers
 @Suppress("MagicNumber")
 val Purple200 = Color(0xFFBB86FC)
+
 @Suppress("MagicNumber")
 val Purple500 = Color(0xFF6200EE)
+
 @Suppress("MagicNumber")
 val Purple700 = Color(0xFF3700B3)
+
 @Suppress("MagicNumber")
 val Teal200 = Color(0xFF03DAC5)
