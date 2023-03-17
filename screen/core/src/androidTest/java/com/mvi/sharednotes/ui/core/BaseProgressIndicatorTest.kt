1fec68e59036d68f3e3aaa4caff0758d6b588d4c
Endorf <dmytro.kosolapenko@gmail.com>
2023-03-17 21:46:02 +0200

    add tests for BaseProgressView

---
+++ screen/core/src/androidTest/java/com/mvi/sharednotes/ui/core/BaseProgressIndicatorTest.kt
@@ -0,0 +1,45 @@
+package com.mvi.sharednotes.ui.core
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.test.hasTestTag
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class BaseProgressIndicatorTest {
+
+    @get: Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun progressView_isVisible() {
+        composeTestRule.apply {
+            setContent {
+                BaseProgressView(
+                    isLoading = true,
+                    modifier = Modifier.fillMaxWidth().testTag(PROGRESS)
+                )
+            }
+
+            onNode(hasTestTag(PROGRESS)).assertExists()
+        }
+    }
+
+    @Test
+    fun progressView_isNotVisible() {
+        composeTestRule.apply {
+            setContent { BaseProgressView(isLoading = false, modifier = Modifier.testTag(PROGRESS)) }
+
+            onNode(hasTestTag(PROGRESS)).assertDoesNotExist()
+        }
+    }
+
+    companion object {
+        private const val PROGRESS = "progress_indicator"
+    }
+}
