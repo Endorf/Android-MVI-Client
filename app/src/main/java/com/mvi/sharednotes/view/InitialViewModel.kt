0f5447dbda3a8c139f4652ec4e36a00bfa397666
Endorf <dmytro.kosolapenko@gmail.com>
2023-03-17 20:02:28 +0200

    emulate initialization

--- app/src/main/java/com/mvi/sharednotes/view/InitialViewModel.kt
+++ app/src/main/java/com/mvi/sharednotes/view/InitialViewModel.kt
@@ -3,16 +3,23 @@ package com.mvi.sharednotes.view
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mvi.sharednotes.view.attributes.InitialState
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
 
-class InitialViewModel : ViewModel() {
+class InitialViewModel(
+) : ViewModel() {
 
-    val state = initialize().stateIn(
+    val state = initialize().map {
+        InitialState.Initialized
+    }.stateIn(
         scope = viewModelScope,
         initialValue = InitialState.Loading,
         started = SharingStarted.WhileSubscribed(5_000),
     )
 
     // todo: replace with initialization from Repository
-    private fun initialize() = flowOf(InitialState.Initialized)
+    private fun initialize() = flow {
+        delay(1000)
+        emit(InitialState.Initialized)
+    }
 }
