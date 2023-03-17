487c5efc165ad96d43f307cce50b24fa2613d8ae
Endorf <dmytro.kosolapenko@gmail.com>
2023-03-17 21:47:34 +0200

    rename class

--- screen/core/src/main/java/com/mvi/sharednotes/ui/core/BaseOutlinedTextField.kt
+++ screen/core/src/main/java/com/mvi/sharednotes/ui/core/BaseOutlinedTextField.kt
@@ -18,7 +18,7 @@ import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun EmailView(
+fun BaseOutlinedTextField(
     value: String,
     isLoading: Boolean,
     hasError: Boolean,
