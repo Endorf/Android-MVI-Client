1b29cf8267d70d583c150557b0e41d2071012b0c
Endorf <dmytro.kosolapenko@gmail.com>
2023-03-17 19:20:38 +0200

    add splashscreen

--- app/src/main/java/com/mvi/sharednotes/SharedNotesActivity.kt
+++ app/src/main/java/com/mvi/sharednotes/SharedNotesActivity.kt
@@ -9,15 +9,19 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import com.mvi.sharednotes.ui.theme.SharedNotesTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
         setContent {
             SharedNotesTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
@@ -27,17 +31,9 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SharedNotesTheme {
-        Greeting("Android")
+    @Composable
+    fun Greeting(name: String) {
+        Text(text = "Hello $name!")
     }
 }
