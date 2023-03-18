73cfe700d1339fad64d746f2531fa726feb18e93
Endorf <dmytro.kosolapenko@gmail.com>
2023-03-18 22:52:59 +0200

    inject Login classes

--- screen/login/src/main/java/com/mvi/sharednotes/login/view/components/middleware/LoggerMiddleware.kt
+++ screen/login/src/main/java/com/mvi/sharednotes/login/view/components/middleware/LoggerMiddleware.kt
@@ -2,8 +2,9 @@ package com.mvi.sharednotes.login.view.components.middleware
 
 import android.util.Log
 import com.mvi.sharednotes.login.view.attributes.Action
+import javax.inject.Inject
 
-class LoggerMiddleware {
+class LoggerMiddleware @Inject constructor() {
 
     fun dispatch(action: Action) {
         Log.e("Login-Logger", "$action")
