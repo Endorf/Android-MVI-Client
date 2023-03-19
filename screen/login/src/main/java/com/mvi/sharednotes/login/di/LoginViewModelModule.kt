4815ec60e36599843f96647ea1a8f0b559a7a9d4
Endorf <dmytro.kosolapenko@gmail.com>
2023-03-19 14:22:23 +0200

    update LoginViewModelModule

--- screen/login/src/main/java/com/mvi/sharednotes/login/di/LoginViewModelModule.kt
+++ screen/login/src/main/java/com/mvi/sharednotes/login/di/LoginViewModelModule.kt
@@ -1,6 +1,6 @@
 package com.mvi.sharednotes.login.di
 
-import com.mvi.sharednotes.login.data.FakeRepository
+import com.mvi.sharednotes.login.data.Repository
 import com.mvi.sharednotes.login.view.components.Reducer
 import com.mvi.sharednotes.login.view.components.middleware.LoggerMiddleware
 import com.mvi.sharednotes.login.view.components.middleware.Middleware
@@ -12,12 +12,12 @@ import dagger.hilt.android.scopes.ViewModelScoped
 
 @Module
 @InstallIn(ViewModelComponent::class)
-class LoginModule {
+class LoginViewModelModule {
 
     @Provides
     @ViewModelScoped
     fun bindMainMiddleware(
-        repository: FakeRepository,
+        repository: Repository,
         logger: LoggerMiddleware
     ) = Middleware(repository, logger)
 
