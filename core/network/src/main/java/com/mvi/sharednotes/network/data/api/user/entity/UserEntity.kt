e20dae82557a9a5e4711b0ade713dc5c5c926a36
Endorf <dmytro.kosolapenko@gmail.com>
2023-03-19 19:30:57 +0200

    update network user entity

--- core/network/src/main/java/com/mvi/sharednotes/network/data/api/user/entity/UserEntity.kt
+++ core/network/src/main/java/com/mvi/sharednotes/network/data/api/user/entity/UserEntity.kt
@@ -1,12 +1,13 @@
 package com.mvi.sharednotes.network.data.api.user.entity
 
 import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class UserEntity(
     val id: Int = -1,
-    var email: String? = null,
+    var email: String,
     val name: String? = null,
-    val userName: String? = null
+    @SerializedName("username") val userName: String? = null
 ) : Parcelable
