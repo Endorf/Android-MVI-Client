4145b97cb402b11735a5446f5540269dedec8304
Endorf <dmytro.kosolapenko@gmail.com>
2023-03-19 14:30:26 +0200

    update middleware

--- screen/login/src/main/java/com/mvi/sharednotes/login/view/components/middleware/Middleware.kt
+++ screen/login/src/main/java/com/mvi/sharednotes/login/view/components/middleware/Middleware.kt
@@ -1,21 +1,20 @@
 package com.mvi.sharednotes.login.view.components.middleware
 
 import androidx.core.util.PatternsCompat
+import com.mvi.sharednotes.login.data.Repository
 import com.mvi.sharednotes.login.view.attributes.Event
 import com.mvi.sharednotes.login.view.attributes.Action
-import com.mvi.sharednotes.login.data.FakeRepository
 import com.mvi.sharednotes.login.view.attributes.State
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.FlowCollector
-import kotlinx.coroutines.flow.flow
+import com.mvi.sharednotes.login.view.entity.UserCredentials
+import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 
 class Middleware @Inject constructor(
-    private val repository: FakeRepository,
+    private val repository: Repository,
     private val loggerMiddleware: LoggerMiddleware
 ) {
 
-    fun dispatch(state: State, event: Event): Flow<Action> = when (event) {
+    suspend fun dispatch(state: State, event: Event): Flow<Action> = when (event) {
         is Event.Login -> login(state)
         is Event.EmailUpdate -> updateInputState(event.email)
     }
@@ -24,14 +23,28 @@ class Middleware @Inject constructor(
         emitAction(Action.Input(email))
     }
 
-    private fun login(state: State) = flow {
-        if (validateEmail(state.email)) {
+    private suspend fun login(state: State): Flow<Action> {
+        return flow {
+            if (validateEmail(state.email).not()) {
+                emitAction(Action.Error)
+                return@flow
+            }
             emitAction(Action.Loading)
 
-            val data = repository.doAuth(state.email)
-            emitAction(Action.SignedIn(data))
-        } else {
-            emitAction(Action.Error)
+            val credentials = UserCredentials(state.email)
+
+            repository.get(credentials)
+                .catch {
+                    emitAction(Action.Loading)
+                    repository.create(credentials)
+                        .catch {
+                            emitAction(Action.Error)
+                        }.collect {
+                            emitAction(Action.SignedIn(it.email))
+                        }
+                }.collect {
+                    emitAction(Action.SignedIn(it.email))
+                }
         }
     }
 
