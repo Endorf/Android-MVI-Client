3e83ab309a3d2f3d55e8cfc654762c6f3c47c2f4
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-06-20 18:12:37 +0300

    update app bg

--- app/src/main/java/com/mvi/sharednotes/view/ui/SharedNotesApp.kt
+++ app/src/main/java/com/mvi/sharednotes/view/ui/SharedNotesApp.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -63,6 +64,7 @@ fun SharedNotesNavHost(
     )
 
     Scaffold(
+        containerColor =MaterialTheme.colorScheme.primaryContainer,
         topBar = {
             SharedNotesAppBar(
                 currentScreen,
