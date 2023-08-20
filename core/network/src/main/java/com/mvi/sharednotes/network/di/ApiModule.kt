03d4a079cf58cddb478eba8f98dbdd8fac567fe0
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-08-20 18:59:17 +0300

    define auth api in ApiModule

--- core/network/src/main/java/com/mvi/sharednotes/network/di/ApiModule.kt
+++ core/network/src/main/java/com/mvi/sharednotes/network/di/ApiModule.kt
@@ -1,6 +1,7 @@
 package com.mvi.sharednotes.network.di
 
-import com.mvi.sharednotes.network.data.api.user.PostApi
+import com.mvi.sharednotes.network.data.api.auth.AuthApi
+import com.mvi.sharednotes.network.data.api.post.PostApi
 import com.mvi.sharednotes.network.data.api.user.UserApi
 import dagger.Module
 import dagger.Provides
@@ -11,6 +12,9 @@ import retrofit2.Retrofit
 @Module
 @InstallIn(ViewModelComponent::class)
 class ApiModule {
+
+    @Provides
+    fun provideAuthApi(retrofit: Retrofit): AuthApi = retrofit.create(AuthApi::class.java)
 
     @Provides
     fun provideUserApi(retrofit: Retrofit): UserApi = retrofit.create(UserApi::class.java)
