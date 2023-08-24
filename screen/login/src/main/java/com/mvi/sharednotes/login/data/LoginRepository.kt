b383917bba1791fed0257c66cb46d0c43a1d4321
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-08-24 13:21:42 +0300

    update repository

--- screen/login/src/main/java/com/mvi/sharednotes/login/data/LoginRepository.kt
+++ screen/login/src/main/java/com/mvi/sharednotes/login/data/LoginRepository.kt
@@ -1,11 +1,15 @@
 package com.mvi.sharednotes.login.data
 
 import com.mvi.sharednotes.login.data.entity.User
-import com.mvi.sharednotes.login.data.entity.mapper.toLocalUserEntity
-import com.mvi.sharednotes.login.data.entity.mapper.toRemoteUserEntity
+import com.mvi.sharednotes.login.data.entity.mapper.toLocalEntity
 import com.mvi.sharednotes.login.data.entity.mapper.toUser
+import com.mvi.sharednotes.login.exception.AuthorizationException
 import com.mvi.sharednotes.login.view.entity.UserCredentials
+import com.mvi.sharednotes.network.data.api.auth.entity.AuthenticationRequest
+import com.mvi.sharednotes.network.data.api.auth.entity.AuthenticationResponse
+import com.mvi.sharednotes.network.data.storage.RemoteAuthDataStore
 import com.mvi.sharednotes.network.data.storage.RemoteUserDataStore
+import com.mvi.sharednotes.storage.AuthDataStore
 import com.mvi.sharednotes.storage.UserDataStore
 import com.mvi.sharednotes.storage.di.qualifier.Local
 import com.mvi.sharednotes.storage.di.qualifier.Shared
@@ -14,26 +18,34 @@ import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class LoginRepository @Inject constructor(
-    private val remoteDataStore: RemoteUserDataStore,
+    private val remoteAuthDataStore: RemoteAuthDataStore,
+    private val remoteUserDataStore: RemoteUserDataStore,
     @Local private val localDataStore: UserDataStore,
-    @Shared private val sharedDataStore: UserDataStore
+    @Shared private val sharedUserDataStore: UserDataStore,
+    @Shared private val sharedAuthDataStore: AuthDataStore
 ) : Repository {
 
-    override suspend fun get(credentials: UserCredentials): Flow<User> = flow {
-        // todo: replace with update
+    override suspend fun signIn(credentials: UserCredentials): Flow<User> = flow {
+        // TODO: move to interceptor
+        val tokenData = provideAuthData(credentials.email, credentials.password)
+        val user = provideUser(tokenData)
 
-        val user = remoteDataStore.read(credentials.toRemoteUserEntity())
-        localDataStore.create(user.toLocalUserEntity())
-//        sharedDataStore.create(user.toLocalUserEntity())
-        emit(user.toUser())
+        emit(user)
     }
 
-    override suspend fun create(credentials: UserCredentials): Flow<User> = flow {
-        remoteDataStore.create(credentials.toRemoteUserEntity())
-            .let { user ->
-                localDataStore.create(user.toLocalUserEntity())
-                sharedDataStore.create(user.toLocalUserEntity())
-                emit(user.toUser())
-            }
+    private suspend fun provideAuthData(email: String, password: String): AuthenticationResponse {
+        val accessTokenResponse: AuthenticationResponse? = remoteAuthDataStore.signIn(
+            AuthenticationRequest(email, password)
+        ).getOrNull()
+
+        accessTokenResponse?.toLocalEntity()?.let {
+            sharedAuthDataStore.put(it)
+        } ?: throw AuthorizationException()
+
+        return accessTokenResponse
+    }
+
+    private suspend fun provideUser(tokenData: AuthenticationResponse): User {
+        return remoteUserDataStore.getCurrentUser(tokenData.access_token).toUser()
     }
 }
