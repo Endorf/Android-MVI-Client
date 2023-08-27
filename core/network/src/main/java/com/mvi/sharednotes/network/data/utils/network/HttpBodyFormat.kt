e4d5b378f7150d82797421a506a86ef361b80fb1
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-08-27 16:25:38 +0300

    refresh token

---
+++ core/network/src/main/java/com/mvi/sharednotes/network/data/utils/network/HttpBodyFormat.kt
@@ -0,0 +1,17 @@
+package com.mvi.sharednotes.network.data.utils.network
+
+import okhttp3.MediaType.Companion.toMediaType
+
+internal enum class HttpBodyFormat {
+    JSON,
+    X_WWW_FORM_URLENCODED;
+
+    companion object {
+
+        fun HttpBodyFormat.toMediaType() = when (this) {
+            JSON -> "application/json"
+            X_WWW_FORM_URLENCODED -> "application/x-www-form-urlencoded"
+        }.toMediaType()
+    }
+}
+
